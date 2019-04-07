941f8414414267574906129034562fd722db922a
Anthony <frostanton09@gmail.com>
2019-04-07 19:10:14 +0300

    End to end connection established

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'frontend';
+
+  constructor(private http:HttpClient){
+
+  }
+
+  getUser():void{
+    this.http.get("/api/user/login/root").subscribe(value => {
+      console.log(value)
+    })
+  }
 }
