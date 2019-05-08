c64c61a1878df794ee309de61211852a93bcc7a6
Anthony <frostanton09@gmail.com>
2019-05-08 12:47:30 +0300

    add security to backend

---
+++ backend/src/main/java/com/edu/bsuir/hotel/hotel/controller/AuthenticationController.java
@@ -0,0 +1,71 @@
+package com.edu.bsuir.hotel.hotel.controller;
+
+import com.edu.bsuir.hotel.hotel.converter.UserToUserDTO;
+import com.edu.bsuir.hotel.hotel.dto.AuthToken;
+import com.edu.bsuir.hotel.hotel.dto.UserDTO;
+import com.edu.bsuir.hotel.hotel.entity.UserEntity;
+import com.edu.bsuir.hotel.hotel.security.TokenProvider;
+import com.edu.bsuir.hotel.hotel.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
+
+import java.security.Principal;
+
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RestController
+@RequestMapping("/api/users/auth")
+public class AuthenticationController {
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private TokenProvider tokenProvider;
+
+    @RequestMapping(value = "/token", method = RequestMethod.POST)
+    public ResponseEntity generateToken(@RequestBody UserEntity loginUser){
+        final Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        loginUser.getLogin(),
+                        loginUser.getPassword()
+                )
+        );
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        final String token = tokenProvider.generateToken(authentication);
+        return ResponseEntity.ok(new AuthToken(token));
+    }
+
+    @PostMapping("/sign-up")
+    public ResponseEntity regNewUser(@RequestBody UserEntity user){
+        UserEntity userResult = userService.save(user);
+        if(userResult==null) return ResponseEntity.badRequest().build();
+
+        final Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        user.getLogin(),
+                        user.getPassword()
+                )
+        );
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        final String token = tokenProvider.generateToken(authentication);
+        return ResponseEntity.ok(new AuthToken(token));
+    }
+
+    @GetMapping("/user")
+    public ResponseEntity<UserDTO> authUser(Principal userInfo){
+        UserToUserDTO converter = new UserToUserDTO();
+        UserEntity user = userService.findByLogin(userInfo.getName());
+        if(user != null){
+            return ResponseEntity.ok(converter.convert(user));
+        }else{
+            return ResponseEntity.notFound().build();
+        }
+    }
+}
