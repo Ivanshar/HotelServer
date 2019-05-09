0487504f928c48892bd5f88f3c69c981ad296f1c
ivshar <ivshar@tut.by>
2019-05-09 16:03:21 +0300

    add valid by BAHO

--- frontend/src/app/modules/individual/components/reviewblank/reviewblank.component.ts
+++ frontend/src/app/modules/individual/components/reviewblank/reviewblank.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {FormControl, Validators} from "@angular/forms";
+import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
 import {ReviewModel} from "../../../../models/review-model";
 import {ReviewService} from "../../../../services/review.service";
 
@@ -12,21 +12,42 @@ export class ReviewblankComponent implements OnInit {
 
   public review: ReviewModel = new ReviewModel();
 
-  constructor(private reviewService: ReviewService) { }
+  reviewForm: FormGroup;
+  submitted = false;
+  form: FormGroup;
 
-  rewiewControl: FormControl;
-  numberControl: FormControl;
-  emailControl: FormControl;
+  constructor(private reviewService: ReviewService, private formBuilder: FormBuilder) { }
+
   ngOnInit() {
-    this.numberControl = new FormControl('', Validators.compose([Validators.required,Validators.min(9999999)])) ;
-    this.emailControl = new FormControl('', Validators.compose([
-      Validators.required,
-      Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$')
-    ]));
-    this.rewiewControl = new FormControl('', Validators.required);
+    // this.numberControl = new FormControl('', Validators.compose([Validators.required,Validators.min(9999999)])) ;
+    // this.emailControl = new FormControl('', Validators.compose([
+    //   Validators.required,
+    //   Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$')
+    // ]));
+    // this.rewiewControl = new FormControl('', Validators.required);
+
+    this.reviewForm = this.formBuilder.group({
+      mark : ['', Validators.required],
+      comment:['', [Validators.required]],
+    });
 
     this.review.mark = 5;
   }
+
+  get f() { return this.reviewForm.controls; }
+
+  onSubmit() {
+    this.submitted = true;
+
+    // stop here if form is invalid
+    if (this.reviewForm.invalid) {
+      return;
+    }
+
+    this.comment();
+
+  }
+
 
   public comment():void{
     this.reviewService.comment(this.review).subscribe(()=>{
